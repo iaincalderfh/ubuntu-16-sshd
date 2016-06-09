6c027c9c3fb63328a4f8ae1d80602b6ddec98a32
Iain Calder <iain.calder@fasthosts.com>
2016-06-09 10:07:05 +0100

    Updated proxy_wrapper

--- files/proxy_wrapper.sh
+++ files/proxy_wrapper.sh
@@ -6,12 +6,18 @@ else
         COMMAND="$@"
 fi
 
-if [[ -t 0 ]]; then
-        echo "tty $COMMAND" >> /tmp/proxylog
-		/sshenv --cafile /ca.crt --tty -i $COMMAND
-else
-        echo "notty $COMMAND" >> /tmp/proxylog
-		/sshenv --cafile /ca.crt -i $COMMAND
-      
-fi
+# TODO get these from user context/FreeIPA
+USERNAME=ssh-49-php
+PROJECT=project-124
 
+export OPENSHIFT_CA_CERT=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+export OPENSHIFT_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+export OPENSHIFT_URL="https://KUBERNETES_SERVICE_HOST:443"
+
+if [[ -t 0 ]]; then
+        #echo "tty $COMMAND" >> /tmp/proxylog
+	/sshenv -i --tty $PROJECT $USERNAME $COMMAND
+else
+        #echo "notty $COMMAND" >> /tmp/proxylog
+	/sshenv -i $PROJECT $USERNAME $COMMAND      
+fi
