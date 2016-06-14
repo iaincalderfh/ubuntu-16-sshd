ce026797b02d9c7ef85d9f3e580e405a9b933643
Iain Calder <iain.calder@fasthosts.com>
2016-06-14 09:46:53 +0100

    Extract username and project name from environment

--- files/proxy_wrapper.sh
+++ files/proxy_wrapper.sh
@@ -6,9 +6,9 @@ else
         COMMAND="$@"
 fi
 
-# TODO get these from user context/FreeIPA
-USERNAME=ssh-49-php
-PROJECT=project-124
+USERNAME=$USER
+# Project group is pg_<project-name>
+PROJECT=$(groups $USER | sed -r 's/.*\bpg_([^\s]*)/\1/')
 
 export OPENSHIFT_CA_CERT=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 export OPENSHIFT_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
